3a7ba2e9fc01514cab4c37160c66a12798fbc657
Muhammad Talha <mtalha@linkedin.com>
2020-01-31 11:23:32 -0800

    Initial commit

--- .vimrc
+++ .vimrc
@@ -1,23 +1,47 @@
+execute pathogen#infect()
+
+filetype plugin indent on
+syntax on
+
+" Vertical line in insert mode
+let &t_ti.="\e[1 q"
+let &t_SI.="\e[5 q"
+let &t_EI.="\e[1 q"
+let &t_te.="\e[0 q"
+
+" Back-space
+set backspace=indent,eol,start
+
+" Dracula colorscheme
+let g:dracula_italic = 0
+colorscheme dracula
+
+set cursorline
+
+" Wildmenu settings
+set wildmenu
+set wildmode=longest:full,full
+
 " <Nop> arrow keys
-cnoremap <Up> <Nop>
-cnoremap <Down> <Nop>
-cnoremap <Left> <Nop>
-cnoremap <Right> <Nop>
+cnoremap <Up> <C-W><C-K>
+cnoremap <Down> <C-W><C-J>
+cnoremap <Left> <C-W><C-H>
+cnoremap <Right> <C-W><C-L>
 
-inoremap <Up> <Nop>
-inoremap <Down> <Nop>
-inoremap <Left> <Nop>
-inoremap <Right> <Nop>
+inoremap <Up> <C-W><C-K>
+inoremap <Down> <C-W><C-J>
+inoremap <Left> <C-W><C-H>
+inoremap <Right> <C-W><C-L>
 
-nnoremap <Up> <Nop>
-nnoremap <Down> <Nop>
-nnoremap <Left> <Nop>
-nnoremap <Right> <Nop>
+nnoremap <Up> <C-W><C-K>
+nnoremap <Down> <C-W><C-J>
+nnoremap <Left> <C-W><C-H>
+nnoremap <Right> <C-W><C-L>
 
-vnoremap <Up> <Nop>
-vnoremap <Down> <Nop>
-vnoremap <Left> <Nop>
-vnoremap <Right> <Nop>
+vnoremap <Up> <C-W><C-K>
+vnoremap <Down> <C-W><C-J>
+vnoremap <Left> <C-W><C-H>
+vnoremap <Right> <C-W><C-L>
 
 " Enable line numbers
 set nu
@@ -31,14 +55,36 @@ set shiftwidth=2
 " Very high scroll-offset to vertically align
 set so=999
 
-" Pathogen 
-execute pathogen#infect()
-
 " Know when Gutentags is generating tags
 set statusline+=%{gutentags#statusline()}
 
 " Hide tag files
 let g:gutentags_cache_dir = expand('~/.tags')
 
-" Dracula colorscheme
-"colorscheme dracula
+" Only index, C, C++, Python files
+let g:gutentags_file_list_command = 'find -regex ".*/.*\.\(c\|cpp\|cc\|hpp\|h\)$"'
+
+" Prevent YCM from asking if its safe to load .ycm_extra_conf.py file
+let g:ycm_confirm_extra_conf = 0
+
+" Prevent YCM from showing preview window
+set completeopt-=preview
+let g:ycm_add_preview_to_completeopt = 0
+
+" Always show left sidebar
+set signcolumn=yes
+
+" Natural splitting
+set splitbelow
+set splitright
+
+set tags+=~/.tags/home-mtalha-dev-ats-core-ats-core_trunk-ats6-src-tags
+set tags+=~/.tags/home-mtalha-dev-ats-core-ats-core_trunk-ats-core-src-tags
+set tags+=~/.tags/home-mtalha-dev-ats-core-ats-core_trunk-tags
+set tags+=~/.tags/home-mtalha-dev-ats-libs-ats-lib-li-atscppapi_trunk-tags
+set tags+=~/.tags/home-mtalha-dev-ats-libs-ats-lib-lixclient_trunk-tags
+set tags+=~/.tags/home-mtalha-dev-ats-libs-ats-lib-lixclient_trunk-tags
+set tags+=~/.tags/home-mtalha-dev-ats-libs-ats-lib-plugin-controls_trunk-tags
+set tags+=~/.tags/home-mtalha-dev-ats-libs-ats-lib-plugin-libs_trunk-tags
+set tags+=~/.tags/home-mtalha-dev-ats-libs-ats-lib-yaml-cpp_trunk-tags
+
---
+++ .ycm_extra_conf.py
@@ -0,0 +1,184 @@
+import platform
+import os
+import json
+import glob
+
+SOURCE_EXTENSIONS = ['.cpp', '.cc', '.c']
+HEADER_EXTENSIONS = ['.h', '.hpp']
+CXX_BLACKLIST_LIBRARIES = ['ats-tester', 'ats-buildfs-rhel7']
+CXX_WHITELIST_LIBRARIES = [('ats-core', ['ats6'], '*.*.*')]
+CXX_ADDITIONAL_LIBRARIES = {'ats-lib-yaml-cpp': ('ATS-lib-li-yamlcpp', [], '*.*.*')}
+PY_LIBRARIES = [('ats-tester', '*.*.*')]
+
+
+def GetUserName():
+    return os.getlogin()
+
+
+def GetLibraryInclude(library, version):
+    os.path.dirname
+    return '/home/%s/native-repo/%s/%s/%s/include' % (GetUserName(), library, library, version)
+
+
+# Compilation flags for C/C++ files
+flags = [
+    '-g',
+    '-pthread',
+    '-Werror',
+    '-Wno-deprecated',
+    '-fPIC',
+    # You 100% do NOT need -DUSE_CLANG_COMPLETER and/or -DYCM_EXPORT in your flags;
+    # only the YCM source code needs it.
+    '-DUSE_CLANG_COMPLETER',
+    '-DYCM_EXPORT=',
+    # Set language (C++)
+    '-x',
+    'c++',
+    # Set library includes
+    '-I',
+    GetLibraryInclude('boost', '1.55.0.6'),
+    '-I',
+    GetLibraryInclude('ats-lib-boost', '1.41.0.0'),
+    # C++11
+    '-std=c++11',
+]
+
+
+def IsHeaderFile(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in HEADER_EXTENSIONS
+
+
+def FindCorrespondingSourceFile(filename):
+    if IsHeaderFile(filename):
+        basename = os.path.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            replacement_file = os.path.dirname(filename) + '/../cpp/' + basename + extension
+            if os.path.exists(replacement_file):
+                return replacement_file
+    return filename
+
+
+def GetHighestVersionLibrary(librarypath, version):
+    if not os.path.exists(librarypath):
+        return None
+    librarypaths = glob.glob(librarypath + '/' + version)
+    librarypaths.sort(reverse=True)
+    if len(librarypaths) == 0:
+        print("Could not resolve: " + librarypath + '/' + version)
+        return None
+    return librarypaths[0] + '/include'
+
+
+def GenerateLibraryIncludes(filename):
+    if not filename.startswith('/home/%s' % GetUserName()):
+        return []
+    basename = os.path.dirname(filename)
+    while basename != '/home/%s' % GetUserName() and not os.path.exists(basename + '/product-spec.json'):
+        basename = os.path.dirname(basename)
+    if basename == '/home/%s' % GetUserName():
+        return []
+    productspec = basename + '/product-spec.json'
+    includes = []
+    additional = []
+    with open(productspec, 'r') as f:
+        json_productspec = json.load(f)
+        for productname in json_productspec['product']:
+            if productname in CXX_BLACKLIST_LIBRARIES:
+                continue
+            if productname in CXX_ADDITIONAL_LIBRARIES:
+                additional.append(CXX_ADDITIONAL_LIBRARIES[productname])
+            libraries = json_productspec['product'][productname]['libraries']
+            libraries.append(productname)
+            version = json_productspec['product'][productname]['version']
+            for library in libraries:
+                libraryinclude = GetHighestVersionLibrary(
+                        '/home/%s/native-repo/com.linkedin.%s/%s' % (GetUserName(), productname, library), version)
+                if libraryinclude is None:
+                    continue
+                includes.append(libraryinclude)
+    includes.extend(GenerateAdditionalLibraryIncludes(additional))
+    return includes
+
+
+def GenerateAdditionalLibraryIncludes(additional):
+    includes = []
+    for (product, libraries, version) in additional:
+        if len(libraries) == 0:
+            libraryinclude = GetHighestVersionLibrary(
+                    '/home/%s/native-repo/com.linkedin.%s/%s' % (GetUserName(), product, product), version)
+            if libraryinclude is not None:
+                includes.append(libraryinclude)
+        else:
+            for library in libraries:
+                libraryinclude = GetHighestVersionLibrary(
+                        '/home/%s/native-repo/com.linkedin.%s/%s' % (GetUserName(), product, library), version)
+                if libraryinclude is not None:
+                    includes.append(libraryinclude)
+    return includes
+
+
+def GenerateLocalInclude(filename):
+    if not filename.startswith('/home/%s' % GetUserName()):
+        return []
+    basename = os.path.dirname(filename)
+    while basename != '/home/%s' % GetUserName() and not os.path.exists(basename + '/product-spec.json'):
+        basename = os.path.dirname(basename)
+    if basename == '/home/%s' % GetUserName():
+        return []
+    includes = []
+    for d in os.walk(basename):
+        include = d[0]
+        if 'release' not in include and ('include' in include or 'cpp' in include or 'src' in include):
+            includes.append(include)
+    return includes
+
+
+def GetPythonInterpreterPath(filename):
+    if not filename.startswith('/home/%s' % GetUserName()):
+        return ''
+    basename = os.path.dirname(filename)
+    while basename != '/home/%s' % GetUserName() and not os.path.exists(basename + '/build'):
+        basename = os.path.dirname(basename)
+    if basename == '/home/%s' % GetUserName():
+        return ''
+    venvpath = basename + '/build/functional_test/venv/bin/python'
+    if not os.path.exists(venvpath):
+        return ''
+    return venvpath
+
+
+def GetPythonSitePackages(filename):
+    if not filename.startswith('/home/%s' % GetUserName()):
+        return ''
+    basename = os.path.dirname(filename)
+    while basename != '/home/%s' % GetUserName() and not os.path.exists(basename + '/build'):
+        basename = os.path.dirname(basename)
+    if basename == '/home/%s' % GetUserName():
+        return ''
+    sitepackages = basename + '/build/functional_test/venv/lib/python3.7/site-packages'
+    if not os.path.exists(sitepackages):
+        return ''
+    return sitepackages
+
+
+def Settings(**kwargs):
+    if not kwargs['filename'].startswith('/home/%s/dev' % GetUserName()):
+        return {}
+    if kwargs['language'] == 'cfamily':
+        filename = FindCorrespondingSourceFile(kwargs['filename'])
+        cxx_flags = flags + ['-I' + include for include in GenerateLocalInclude(filename) + GenerateLibraryIncludes(filename) + GenerateAdditionalLibraryIncludes(CXX_WHITELIST_LIBRARIES)]
+        print(cxx_flags)
+        return {
+            'flags': cxx_flags,
+            'override_filename': filename
+        }
+    elif kwargs['language'] == 'python':
+        return {
+            'interpreter_path': GetPythonInterpreterPath(kwargs['filename']),
+            'sys_path': [
+                GetPythonSitePackages(kwargs['filename'])
+            ]
+        }
+    return {}
+
